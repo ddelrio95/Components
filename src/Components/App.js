6a46f32d650a0986f505fb2ab1959120dc058e0a
Dario <57421573+ddelrio95@users.noreply.github.com>
2022-01-17 07:58:48 -0500

    header-component

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+
+export default function App() {
+  return (
+    <div className='Greeting'>
+      <Header />
+      {/* <Comp1 /> */}
+    </div>
+  )
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <a class='active' href='#home'>
+        Home
+      </a>
+      <a class='active' href='#about'>
+        About
+      </a>
+      <a class='active' href='#projects'>
+        Projects
+      </a>
+      <a class='active' href='#help'>
+        Help
+      </a>
+    </div>
+  )
+}
+
+export default Header
