21f0440b59d2b93bbf080b6ee0ece86261f64e7c
Dario <57421573+ddelrio95@users.noreply.github.com>
2022-01-21 07:52:21 -0500

    clean

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,15 +2,12 @@ import React from 'react'
 import './App.css'
 import Header from './Header'
 import Footer from './Footer'
-import Cards from './Cards'
 
 // Functional Component
 function App() {
   return (
-    <div className='info'>
+    <div className='header'>
       <Header />
-      <Cards name='Dario' heroName='Software Engineer' />
-      <Cards name='Chely' heroName='Nurse' />
       <Footer />
     </div>
   )
